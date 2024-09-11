45f076c394fec1f420c6cd53bbc1fd6d149e07d0
Dilan <dilan.p.2020.ad@ritchennai.edu.in>
2024-09-11 08:09:40 +0530

    intiial commit

--- app.py
+++ app.py
@@ -234,4 +234,5 @@ def score_resume():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
